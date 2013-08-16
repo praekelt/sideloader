d43e9881c3a9cd4f356cbda078f9abe25bfda74e
Colin Alston <colin.alston@gmail.com>
2013-08-16 08:57:54 +0000

    Remove more examples...

--- skeleton/settings.py
+++ skeleton/settings.py
@@ -188,7 +188,9 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 # CELERY_ALWAYS_EAGER = DEBUG
 
 # Tell Celery where to find the tasks
-CELERY_IMPORTS = ('celery_app.tasks',)
+CELERY_IMPORTS = (
+    #'celery_app.tasks',
+)
 
 # Defer email sending to Celery, except if we're in debug mode,
 # then just print the emails to stdout for debugging.
--- skeleton/urls.py
+++ skeleton/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^celery/', include('celery_app.urls')),
+    #url(r'^celery/', include('celery_app.urls')),
 )
