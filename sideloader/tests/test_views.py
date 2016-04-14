325330426bc4c40fa54c8cacece6b0a2f4e78359
Jeremy Thurgood <jeremy@praekelt.com>
2016-04-14 19:12:27 +0200

    Some release flow view tests.

--- sideloader/tests/test_views.py
+++ sideloader/tests/test_views.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 from sideloader.models import Project, ReleaseFlow, ReleaseStream
+
+
 class TestIndex(TestCase):
     def test_auth_required(self):
         """
@@ -27,7 +29,6 @@ class TestIndex(TestCase):
 
 class TestProject(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user("me", "me@example.com", "pass")
         self.root = User.objects.create_superuser(
             "root", "root@localhost", "pass")
 
@@ -62,8 +63,8 @@ class TestProject(TestCase):
         """
         When we create a new project, we get some default workflows.
         """
-        prod_s = ReleaseStream.objects.create(name="Production")
-        qa_s = ReleaseStream.objects.create(name="QA")
+        prod_stream = ReleaseStream.objects.create(name="Production")
+        qa_stream = ReleaseStream.objects.create(name="QA")
         self.client.login(username="root", password="pass")
         self.assertEqual(list(Project.objects.all()), [])
         self.assertEqual(list(ReleaseFlow.objects.all()), [])
@@ -78,6 +79,51 @@ class TestProject(TestCase):
 
         [prod_f, qa_f] = list(ReleaseFlow.objects.all().order_by("name"))
         self.assertEqual(prod_f.project, proj)
-        self.assertEqual(prod_f.stream, prod_s)
+        self.assertEqual(prod_f.stream, prod_stream)
         self.assertEqual(qa_f.project, proj)
-        self.assertEqual(qa_f.stream, qa_s)
+        self.assertEqual(qa_f.stream, qa_stream)
+
+
+class TestReleaseFlow(TestCase):
+    def setUp(self):
+        self.root = User.objects.create_superuser(
+            "root", "root@localhost", "pass")
+        self.prod_stream = ReleaseStream.objects.create(name="Production")
+        self.qa_stream = ReleaseStream.objects.create(name="QA")
+        self.proj = Project.objects.create(
+            name="My Project", github_url="foo.git", branch="develop",
+            created_by_user=self.root, idhash="seekrit")
+
+    def test_new_flow_stream(self):
+        """
+        We can create a new stream-mode release flow for a project.
+        """
+        self.client.login(username="root", password="pass")
+        self.assertEqual(list(ReleaseFlow.objects.all()), [])
+        url = reverse("workflow_create", args=[self.proj.pk])
+        resp = self.client.post(url, {
+            "name": "My Flow",
+            "stream_mode": 0,
+        })
+        [flow] = ReleaseFlow.objects.all()
+        self.assertEqual(flow.name, "My Flow")
+        self.assertEqual(flow.stream_mode, 0)
+        self.assertRedirects(
+            resp, reverse("projects_view", args=[self.proj.pk]))
+
+    def test_new_flow_server(self):
+        """
+        We can create a new server-mode release flow for a project.
+        """
+        self.client.login(username="root", password="pass")
+        self.assertEqual(list(ReleaseFlow.objects.all()), [])
+        url = reverse("workflow_create", args=[self.proj.pk])
+        resp = self.client.post(url, {
+            "name": "My Flow",
+            "stream_mode": 1,
+        })
+        [flow] = ReleaseFlow.objects.all()
+        self.assertEqual(flow.name, "My Flow")
+        self.assertEqual(flow.stream_mode, 1)
+        self.assertRedirects(
+            resp, reverse("projects_view", args=[self.proj.pk]))
